407b290eabd0d29e058efacc0211655109ecc91b
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2019-08-01 14:37:42 -0400

    removed sticky header

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-const addSticky = () => {
-  let header = document.getElementById("header");
-  let sticky = header.offsetTop;
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-};
-
 const playSoundOnClick = () => {
   let audio = document.getElementById("button_sound");
   audio.play();
 };
-
-window.onscroll = addSticky;
